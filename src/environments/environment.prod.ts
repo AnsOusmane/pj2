78961d3720cd3427c345edd613ca411a2bfb1e62
Ansousmane <ndiayeansoumana96@gmail.com>
2026-05-13 10:43:33 +0000

    add new prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-
- 
-  apiBaseUrl: 'https://sencsu.sn/api',
-
-  // Images / uploads
-  mediaBaseUrl: 'https://sencsu.sn'
+  apiBaseUrl: 'https://sencsu-backend.onrender.com/api',
+  mediaBaseUrl: 'https://sencsu-backend.onrender.com'
 };
