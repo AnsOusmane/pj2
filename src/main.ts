4f110ebe6106e43a799314dc0e17d5c88cdb7b32
Ansousmane <ndiayeansoumana96@gmail.com>
2026-06-12 19:32:55 +0000

    corrections

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 
 import { App } from './app/app';
-import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(App, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient() // 👈 OBLIGATOIRE
-  ]
-})
+bootstrapApplication(App, appConfig)
 .catch(err => console.error(err));
